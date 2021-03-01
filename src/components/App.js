991175bbb1ec5d935d0b39cfbaea075c4ffc9432
wrack <carlosveiga1618@gmail.com>
2021-03-01 17:43:25 -0300

    components/App: change app component to a class, add props and set stat, refactor rendering and add handleClick function to calculate what is needed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
+import { render } from '@testing-library/react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/operate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel calculate={calculate} />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    this.setState(state => calculate(state, buttonName));
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <>
+        <Display result={!total ? total : next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
+}
 
 export default App;
