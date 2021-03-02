9d6ac26820116ebbff3e1e007d3881fa58737e4d
wrack <carlosveiga1618@gmail.com>
2021-03-02 13:01:45 -0300

    logic/calculate: rename variables to make it more understandable

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,7 @@
 import operate from './operate';
 
-const calculate = (data, buttonName) => {
-  let { total, next, operation } = data;
+const calculate = (values, buttonName) => {
+  let { total, next, operation } = values;
   const numbers = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   if (numbers.includes(buttonName)) {
