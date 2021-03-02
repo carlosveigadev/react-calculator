4bcc94685d6fa8de81a428e41919da6cdd67023f
wrack <carlosveiga1618@gmail.com>
2021-03-02 13:00:06 -0300

    components/App: refactor code to add useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [values, setValues] = useState({ total: null, next: null, operation: null });
 
-    this.handleClick = this.handleClick.bind(this);
+  const handleClick = buttonName => {
+    setValues(calculate(values, buttonName));
+  };
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+  const { total, next } = values;
+
+  let showResult;
+  if (next !== null) {
+    showResult = next;
+  } else if (total !== null) { showResult = total; } else {
+    showResult = '0';
   }
 
-  handleClick(buttonName) {
-    this.setState(state => {
-      const { total, next, operation } = state;
-      return calculate({ total, next, operation }, buttonName);
-    });
-  }
-
-  render() {
-    const { total, next } = this.state;
-
-    let showResult;
-    if (next !== null) {
-      showResult = next;
-    } else if (total !== null) { showResult = total; } else {
-      showResult = '0';
-    }
-
-    return (
-      <>
-        <Display result={showResult} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Display result={showResult} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
