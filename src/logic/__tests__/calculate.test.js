9656fa31dbc310b4e167272d8218ebd27e4e2da1
wrack <carlosveiga1618@gmail.com>
2021-03-04 13:31:36 -0300

    logic/__tests__/calculate.test:  test major 4 operations, test other default cases updating the values of the object passed.

--- src/logic/__tests__/calculate.test.js
+++ src/logic/__tests__/calculate.test.js
@@ -1,9 +1,45 @@
 import calculate from '../calculate';
 
 describe('test the calculate function that makes the major calculations for the app', () => {
-  const values = { total: 5, next: 5, operation: null };
+  describe('test default operations', () => {
+    const values = { total: '5', next: '5', operation: null };
+    let buttonName = null;
+    it('resets the values of next and operation', () => {
+      values.next = '0';
+      values.operation = '÷';
+      const resultObject = calculate(values);
+      expect(resultObject.next).toBe(null);
+      expect(resultObject.operation).toBe(null);
+      expect(resultObject.total).toBe('Error, You could destroy the world if you divide by 0.');
+    });
+
+    it('makes the value of number to be negative, using -, when operation is null', () => {
+      values.next = null;
+      values.operation = null;
+      buttonName = '-';
+      const resultObject = calculate(values, buttonName);
+      expect(resultObject.next).toBe(buttonName);
+    });
+
+    it('makes the value of number to be negative, using -, when operation is "÷"', () => {
+      values.next = null;
+      values.operation = '÷';
+      buttonName = '-';
+      const resultObject = calculate(values, buttonName);
+      expect(resultObject.next).toBe(buttonName);
+    });
+
+    it('makes the value of number to be negative, using -, when operation is "x"', () => {
+      values.next = null;
+      values.operation = 'x';
+      buttonName = '-';
+      const resultObject = calculate(values, buttonName);
+      expect(resultObject.next).toBe(buttonName);
+    });
+  });
 
   describe('test major 4 operators', () => {
+    const values = { total: '5', next: '5', operation: null };
     test('+ operator', () => {
       values.operation = '+';
       const resultObject = calculate(values);
@@ -29,5 +65,21 @@ describe('test the calculate function that makes the major calculations for the 
     });
   });
 
-  describe('');
+  describe('other default elements that update the values of the object.', () => {
+    const values = { total: null, next: null, operation: '-' };
+    it('returns the value of total as the value of next and update next to be equal to null, also updates the value of operation with the buttonName operator', () => {
+      values.next = '5';
+      const buttonName = '+';
+      const resultObject = calculate(values, buttonName);
+      expect(resultObject.operation).toBe(buttonName);
+      expect(resultObject.total).toBe(values.next);
+      expect(resultObject.next).toBe(null);
+    });
+
+    it('updates the value of operation to be equal to buttonName when no other values are passed.', () => {
+      const buttonName = '+';
+      const resultObject = calculate(values, buttonName);
+      expect(resultObject.operation).toBe(buttonName);
+    });
+  });
 });
