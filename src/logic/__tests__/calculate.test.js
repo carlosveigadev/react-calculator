583c87cbc9673c80bad214e2fb67a634c44c5895
wrack <carlosveiga1618@gmail.com>
2021-03-04 12:50:41 -0300

    logic/__tests__/calculate.test: refactor code to make it more understandable

--- src/logic/__tests__/calculate.test.js
+++ src/logic/__tests__/calculate.test.js
@@ -3,27 +3,31 @@ import calculate from '../calculate';
 describe('test the calculate function that makes the major calculations for the app', () => {
   const values = { total: 5, next: 5, operation: null };
 
-  test('Test + operator', () => {
-    values.operation = '+';
-    const resultObject = calculate(values);
-    expect(resultObject.total).toBe('10');
+  describe('test major 4 operators', () => {
+    test('+ operator', () => {
+      values.operation = '+';
+      const resultObject = calculate(values);
+      expect(resultObject.total).toBe('10');
+    });
+
+    test('- operator', () => {
+      values.operation = '-';
+      const resultObject = calculate(values);
+      expect(resultObject.total).toBe('0');
+    });
+
+    test('÷ operator', () => {
+      values.operation = '÷';
+      const resultObject = calculate(values);
+      expect(resultObject.total).toBe('1');
+    });
+
+    test('x operator', () => {
+      values.operation = 'x';
+      const resultObject = calculate(values);
+      expect(resultObject.total).toBe('25');
+    });
   });
 
-  test('Test - operator', () => {
-    values.operation = '-';
-    const resultObject = calculate(values);
-    expect(resultObject.total).toBe('0');
-  });
-
-  test('Test ÷ operator', () => {
-    values.operation = '÷';
-    const resultObject = calculate(values);
-    expect(resultObject.total).toBe('1');
-  });
-
-  test('Test x operator', () => {
-    values.operation = 'x';
-    const resultObject = calculate(values);
-    expect(resultObject.total).toBe('25');
-  });
+  describe('');
 });
